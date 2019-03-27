f89bc681d948cb0d4fd59a69fd5b80d4b36d1183
mar444 <mark.zhouxili@gmail.com>
2019-03-26 23:51:09 -0700

    wip

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -3,25 +3,35 @@ package raftkv
 import "labrpc"
 import "crypto/rand"
 import "math/big"
+import "sync"
 
 
 type Clerk struct {
-	servers []*labrpc.ClientEnd
-	// You will have to modify this struct.
+    servers []*labrpc.ClientEnd
+    // You will have to modify this struct.
+    mu sync.Mutex
+    id int64
+    seq int
+    leaderIndex int
 }
 
 func nrand() int64 {
-	max := big.NewInt(int64(1) << 62)
-	bigx, _ := rand.Int(rand.Reader, max)
-	x := bigx.Int64()
-	return x
+    max := big.NewInt(int64(1) << 62)
+    bigx, _ := rand.Int(rand.Reader, max)
+    x := bigx.Int64()
+    return x
 }
 
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
-	ck := new(Clerk)
-	ck.servers = servers
-	// You'll have to add code here.
-	return ck
+    ck := new(Clerk)
+    ck.servers = servers
+    // You'll have to add code here.
+
+    ck.id = nrand()
+    ck.seq = 0
+    ck.leaderIndex = 0
+
+    return ck
 }
 
 //
@@ -38,8 +48,51 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 //
 func (ck *Clerk) Get(key string) string {
 
-	// You will have to modify this function.
-	return ""
+    // You will have to modify this function.
+    ck.mu.Lock()
+    ck.seq++
+
+    seq := ck.seq
+    idx := ck.leaderIndex
+    ck.mu.Unlock()
+
+    i := idx
+
+    for {
+        args := GetArgs{
+            Key: key,
+            Id: ck.id,
+            Seq: seq,
+        }
+
+        reply := GetReply{}
+        DPrintf("%v %v send GET RPC for %v key: %v\n", ck.id, seq, i, key)
+        ok := ck.servers[i].Call("KVServer.Get", &args, &reply)
+
+        if ok {
+            if !reply.WrongLeader {
+                ck.mu.Lock()
+                ck.leaderIndex = i
+                ck.mu.Unlock()
+
+                if reply.Err == "" {
+                    DPrintf("%v %v get Get succeeded result from server %v %v.\n", ck.id, seq, key, reply.Value)
+                    return reply.Value
+                } else if reply.Err == ErrNoKey {
+                    DPrintf("%v %v get no key error result from server %v.\n", ck.id, seq, key)
+                    return ""
+                } else {
+                    DPrintf("%v %v get Get error result from server %v.\n", ck.id, seq, reply.Err)
+                }
+            } else {
+                DPrintf("%v %v wrong leader\n", ck.id, seq)
+            }
+            
+        } else {
+            DPrintf("%v %v RPC failed to %v\n", ck.id, seq, i)
+        }
+        i = (i + 1) % len(ck.servers)
+    }
 }
 
 //
@@ -53,12 +106,60 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
-	// You will have to modify this function.
+    // You will have to modify this function.
+
+    ck.mu.Lock()
+    ck.seq++
+
+    seq := ck.seq
+    idx := ck.leaderIndex
+    ck.mu.Unlock()
+
+    i := idx
+
+    for {
+        args := PutAppendArgs{
+            Key: key,
+            Value: value,
+            Op: op,
+            Id: ck.id,
+            Seq: seq,
+        }
+
+        reply := PutAppendReply{}
+
+        DPrintf("%v %v send PUT RPC for %v key: %v val: %v op: %v\n", ck.id, seq, i, key, value, op)
+        ok := ck.servers[i].Call("KVServer.PutAppend", &args, &reply)
+
+        if ok {
+            if !reply.WrongLeader {
+                ck.mu.Lock()
+                ck.leaderIndex = i
+                ck.mu.Unlock()
+
+                if reply.Err == "" {
+                    DPrintf("%v %v get succeeded result from server.\n", ck.id, seq)
+                    return
+                } else if reply.Err == ErrDuplicate {
+                    DPrintf("%v %v get duplicate error result from server %v.\n", ck.id, seq, reply.Err)
+                    return
+                } else {
+                    DPrintf("%v %v get error result from server %v.\n", ck.id, seq, reply.Err)
+                }
+            } else {
+                DPrintf("%v %v wrong leader\n", ck.id, seq)
+            }
+            
+        } else {
+            DPrintf("%v %v RPC failed to %v\n", ck.id, seq, i)
+        }
+        i = (i + 1) % len(ck.servers)
+    }
 }
 
 func (ck *Clerk) Put(key string, value string) {
-	ck.PutAppend(key, value, "Put")
+    ck.PutAppend(key, value, "Put")
 }
 func (ck *Clerk) Append(key string, value string) {
-	ck.PutAppend(key, value, "Append")
+    ck.PutAppend(key, value, "Append")
 }
